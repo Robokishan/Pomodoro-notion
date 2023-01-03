98ab173aa152f6a5c82c7f884d50ebdaeba909c5
Kishan Joshi <robokishan.blogspot@gmail.com>
2023-01-03 11:23:34 +0530

    feat: 🎸 add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 import { env } from "./src/env/server.mjs";
+import WithPWA from "next-pwa";
 
+const withPWA = WithPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -12,48 +18,50 @@ function defineNextConfig(config) {
   return config;
 }
 
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-    ],
-  },
-  webpack(config, options) {
-    const { isServer } = options;
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      use: {
-        loader: "file-loader",
-        options: {
-          publicPath: `/_next/static/audio/`,
-          outputPath: `${isServer ? "../" : ""}static/audio/`,
-          name: "[name]-[hash].[ext]",
-          esModule: config.esModule || false,
+export default withPWA(
+  defineNextConfig({
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com",
         },
-      },
-    });
-    return config;
-  },
+        {
+          protocol: "https",
+          hostname: "picsum.photos",
+        },
+      ],
+    },
+    webpack(config, options) {
+      const { isServer } = options;
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: `/_next/static/audio/`,
+            outputPath: `${isServer ? "../" : ""}static/audio/`,
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      });
+      return config;
+    },
 
-  async rewrites() {
-    return [
-      {
-        source: "/notion/:path*",
-        destination: "https://api.notion.com/:path*",
-      },
-    ];
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-});
+    async rewrites() {
+      return [
+        {
+          source: "/notion/:path*",
+          destination: "https://api.notion.com/:path*",
+        },
+      ];
+    },
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+  })
+);
