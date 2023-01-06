411614576493adf2913c04946962852f427bcc52
Kishan Joshi <robokishan.blogspot@gmail.com>
2023-01-06 19:40:08 +0530

    fix: 🐛 now use session email rather then relying on notion stat

--- src/pages/api/notion/oauth/index.ts
+++ src/pages/api/notion/oauth/index.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 import { showError } from "../../../../utils/apis";
 import { createNotionUser } from "../../../../utils/apis/firebase/userNotion";
 
@@ -12,6 +13,8 @@ export default async function handler(
     if (method == "GET") {
       const { code, state } = req.query;
       if (!code || !state) throw new Error("No code or state found");
+      const session = await getSession({ req });
+      if (!session?.user?.email) throw new Error("No session found");
       const { data } = await axios.post(
         "https://api.notion.com/v1/oauth/token",
         {
@@ -30,11 +33,10 @@ export default async function handler(
       const { access_token, ...workspaceData } = data;
       await createNotionUser({
         accessToken: access_token,
-        email: state as string,
+        email: session.user.email,
         workspace: workspaceData,
       });
       res.redirect("/");
-      // res.status(200).json(data);
     } else {
       res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
