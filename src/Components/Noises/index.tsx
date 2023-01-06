cb63dbdee5f2a82d9f6f4ce5d9970c015b5f029a
Kishan Joshi <robokishan.blogspot@gmail.com>
2023-01-06 20:05:40 +0530

    style: 💄 change noise container grid

--- src/Components/Noises/index.tsx
+++ src/Components/Noises/index.tsx
@@ -3,7 +3,7 @@ import NoiseCard from "./NoiseCard";
 
 export default function Noises() {
   return (
-    <div className="mx-auto mt-10 grid w-fit grid-cols-[repeat(4,6rem)] place-content-center place-items-center gap-10 ">
+    <div className="mx-auto mt-10 grid w-fit grid-cols-[repeat(4,auto)] place-content-center place-items-center gap-10 ">
       {noises.map((noise) => (
         <NoiseCard
           key={noise.value}
