304c0738e329c399a1d9b75d9d11300e1194b625
Kishan Joshi <robokishan.blogspot@gmail.com>
2023-01-28 14:31:10 +0530

    fix: 🐛 pagination for pages list and database list

--- src/utils/apis/notion/database.ts
+++ src/utils/apis/notion/database.ts
@@ -11,17 +11,42 @@ export const queryDatabase = async (
   serverSide = false,
   token = ""
 ): Promise<DatabaseQuery> => {
+  return await fetchDatabasePages(id, serverSide, token);
+};
+
+const fetchDatabasePages = async (
+  id: string,
+  serverSide = false,
+  token = "",
+  next_cursor: string | undefined = undefined
+): Promise<DatabaseQuery> => {
   const { data } = serverSide
-    ? await notionClient.post(
+    ? await notionClient.post<DatabaseQuery>(
         BASE_DATABASE + "/" + id + "/query",
-        {},
+        next_cursor != undefined || next_cursor != null
+          ? {
+              start_cursor: next_cursor,
+            }
+          : {},
         {
           headers: {
             Authorization: "Bearer " + token,
           },
         }
       )
-    : await NotionClient.post(BASE_DATABASE + "/" + id + "/query");
+    : await NotionClient.post<DatabaseQuery>(
+        BASE_DATABASE + "/" + id + "/query"
+      );
+
+  if (data.has_more) {
+    const nextData = await fetchDatabasePages(
+      id,
+      serverSide,
+      token,
+      data.next_cursor
+    );
+    data.results = data.results.concat(nextData.results);
+  }
   return data;
 };
 
@@ -44,12 +69,29 @@ export const listDatabases = async (
   serverSide = false,
   token = ""
 ): Promise<DatabaseList> => {
+  return await fetchDatabases(serverSide, token);
+};
+
+const fetchDatabases = async (
+  serverSide = false,
+  token = "",
+  next_cursor: string | undefined | null = undefined
+) => {
   const { data } = serverSide
-    ? await notionClient.get(BASE_DATABASE, {
+    ? await notionClient.get<DatabaseList>(BASE_DATABASE, {
         headers: {
           Authorization: "Bearer " + token,
         },
+        params: {
+          start_cursor: next_cursor,
+        },
       })
-    : await NotionClient.get(BASE_DATABASE);
+    : await NotionClient.get<DatabaseList>(BASE_DATABASE);
+
+  if (data.has_more && data.results) {
+    const nextData = await fetchDatabases(serverSide, token, data.next_cursor);
+    if (nextData.results) data.results = data.results.concat(nextData.results);
+  }
+
   return data;
 };
